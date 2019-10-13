e5796ce32be16e01da26fd884d22f9548c22dba0
debdutgoswami <debdutgoswami@gmail.com>
2019-10-14 02:17:59 +0530

    DOCUMENTATION Q7.py

--- Question 1-10/Q7/Q7.py
+++ Question 1-10/Q7/Q7.py
@@ -1,5 +1,13 @@
 
 def findSingle(nums):
+    """this function finds the element which is not repeated twice
+    
+    Arguments:
+        nums {list} -- list of integers obtained as input
+    
+    Returns:
+        int -- the element
+    """
     res = 0
     for i in nums:
         res = res ^ i
